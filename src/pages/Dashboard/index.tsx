162e8694ae05d12f63883ff811b740cace52c449
Fabio Menozzi <f.menozzi@yahoo.com>
2020-10-01 18:52:14 -0300

    Dashboard styles in progress

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import {  FiChevronRight } from 'react-icons/fi'
 
 import logoImg from '../../assets/logoGit.svg';
+import Repository from '../Repository';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
     return (
@@ -14,6 +16,48 @@ const Dashboard: React.FC = () => {
                 <input type="text" placeholder='Type the repository name' />
                 <button type='submit'>Search</button>
             </Form>
+
+            <Repositories>
+                <a href='test'>
+                    <img
+                        src="https://avatars1.githubusercontent.com/u/60794803?s=400&u=d0efdbf4cbeb23895315447ed2a0c02751ed1fc2&v=4"
+                        alt="Fabio Menozzi"
+                    />
+                    <div>
+                        <strong>menozzi/menozzi</strong>
+                        <p>description description</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href='test'>
+                    <img
+                        src="https://avatars1.githubusercontent.com/u/60794803?s=400&u=d0efdbf4cbeb23895315447ed2a0c02751ed1fc2&v=4"
+                        alt="Fabio Menozzi"
+                    />
+                    <div>
+                        <strong>menozzi/menozzi</strong>
+                        <p>description description</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href='test'>
+                    <img
+                        src="https://avatars1.githubusercontent.com/u/60794803?s=400&u=d0efdbf4cbeb23895315447ed2a0c02751ed1fc2&v=4"
+                        alt="Fabio Menozzi"
+                    />
+                    <div>
+                        <strong>menozzi/menozzi</strong>
+                        <p>description description</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+
         </>
     );
 };
