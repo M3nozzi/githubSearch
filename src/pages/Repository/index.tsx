c23fee3345e3c3482fee82487c4637273fd4ffa7
Fabio Menozzi <f.menozzi@yahoo.com>
2020-10-03 15:38:08 -0300

    repository route

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft } from 'react-icons/fi';
+
+import logoImg from '../../assets/logoGit.svg';
+import { Header } from './styles';
+
+interface RepositoryParams {
+    repository: string;
+}
 
 const Repository: React.FC = () => {
-    return <h1>Repository</h1>;
+    const { params } = useRouteMatch<RepositoryParams>();
+
+    return (
+        <Header>
+            <img src={logoImg} alt='Github Search' />
+            <Link to='/dashboard'>
+                <FiChevronLeft size={16} />
+                Home
+            </Link>
+        </Header>
+    );
 };
 
 export default Repository;
